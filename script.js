5ce7c6f3febedb65c90a255955412de29f0fe4e8
Kailo Santos <105285640+kailosantos1@users.noreply.github.com>
2022-12-02 16:15:09 -0300

    Projeto-Prompt-JS-Kailo_Santos-

---
+++ script.js
@@ -0,0 +1,14 @@
+var nome = prompt("Insira seu nome ");
+var idade = prompt("Insira sua idade");
+var loc = prompt("Insira sua localização ");
+
+document.write("Hello " + nome, " seja bem vind@");
+document.write("<br>");
+document.write("Você tem " + idade, " anos, está nov@ ainda!");
+document.write("<br>");
+document.write("Você mora em " + loc);
+
+
+console.log("Seu nome é: " + nome);
+console.log("Sua idade é: " + idade);
+console.log("Você mora em: " + loc);
